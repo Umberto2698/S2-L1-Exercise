206aa18baf72cbbc6cf72890b182a08bf02f4b7b
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-03 18:02:50 +0200

    Tolti i commenti

--- U1-W2-D1/D1.js
+++ U1-W2-D1/D1.js
@@ -20,42 +20,47 @@ REGOLE
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* let name = "Umberto"; */
+let name = "Umberto";
+console.log(name);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* let numero1 = 12;
-   let numero2 = 20;
-   let somma = numero1 + numero2; */
+let numero1 = 12;
+let numero2 = 20;
+let somma = numero1 + numero2;
+console.log(somma);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* let x = 12; */
+let x = 12;
+comsole.log(x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* name = "Zappulla";
+name = "Zappulla";
+console.log(name);
 
-   Se avessi scritto:
+/*Se avessi scritto:
 
-    const name = "Umberto" 
-    name = 'Zappulla' 
+    const name = "Umberto"; 
+    name = 'Zappulla'; 
     console.log(name);
 
-  Avrei ottenuto un'errore alla riga 49 */
+Avrei ottenuto un'errore alla riga 53 */
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* let sottrazione = 4 - x;*/
+let sottrazione = 4 - x;
+console.log(sottrazione);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -63,6 +68,9 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* let name1 = "john"; 
-   let name2 = "John";
-   let verifica = name1 === name2 || name1.toLowerCase === name2.toLowerCase;*/
+let name1 = "john";
+let name2 = "John";
+let verifica = name1 === name2;
+console.log(verifica);
+let extra = name1.toLowerCase === name2.toLowerCase;
+console.log(extra);
